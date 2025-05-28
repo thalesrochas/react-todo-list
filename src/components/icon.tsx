d86992d73a6aaac29506ad551ad1d238b69b7d23
Thales Rocha <thales_rochasousa@hotmail.com>
2025-05-28 00:32:32 -0300

    Adiciona suporte ao vite-plugin-svgr no projeto, atualizando os arquivos package.json e vite.config.ts. Inclui novas definições de tipos no arquivo vite-env.d.ts e cria componente Icon para manipulação de ícones SVG. Atualiza ícone SVG existente em src/assets/icons/x.svg.

---
+++ src/components/icon.tsx
@@ -0,0 +1,30 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import type React from "react";
+
+export const iconVariants = cva("", {
+  variants: {
+    animate: {
+      true: "animate-spin",
+      false: "",
+    },
+  },
+  defaultVariants: {
+    animate: false,
+  },
+});
+
+type IconProps = {
+  svg: React.FC<React.ComponentProps<"svg">>;
+} & React.ComponentProps<"svg"> &
+  VariantProps<typeof iconVariants>;
+
+export default function Icon({
+  animate,
+  className,
+  svg: SvgComponent,
+  ...props
+}: IconProps) {
+  return (
+    <SvgComponent className={iconVariants({ animate, className })} {...props} />
+  );
+}

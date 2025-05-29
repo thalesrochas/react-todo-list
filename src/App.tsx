d896a1a9e8c7eb9534d087eb319626aad58e6a3f
Thales Rocha <thales_rochasousa@hotmail.com>
2025-05-29 02:11:46 -0300

    Adiciona suporte ao react-router, implementa rotas para as páginas Home e Componentes, e cria novos componentes LayoutMain, PageHome e PageComponents.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import LayoutMain from "./pages/layout-main";
+import PageComponents from "./pages/page-components";
+import PageHome from "./pages/page-home";
+
 export default function App() {
-  return <>Hello world!</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<LayoutMain />}>
+          <Route index element={<PageHome />} />
+          <Route path="/componentes" element={<PageComponents />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/pages/page-components.tsx
@@ -0,0 +1,82 @@
+import Badge from "../components/badge";
+import ButtonIcon from "../components/buton-icon";
+import Button from "../components/button";
+import Card from "../components/card";
+import Container from "../components/container";
+import Icon from "../components/icon";
+import InputCheckbox from "../components/input-checkbox";
+import InputText from "../components/input-text";
+import Skeleton from "../components/skeleton";
+import Text from "../components/text";
+
+import CheckIcon from "../assets/icons/check.svg?react";
+import PencilIcon from "../assets/icons/pencil.svg?react";
+import PlusIcon from "../assets/icons/plus.svg?react";
+import SpinnerIcon from "../assets/icons/spinner.svg?react";
+import TrashIcon from "../assets/icons/trash.svg?react";
+import XIcon from "../assets/icons/x.svg?react";
+
+export default function PageComponents() {
+  return (
+    <Container>
+      <div className="grid gap-10">
+        <div className="flex flex-col gap-1">
+          <Text variant="body-sm-bold" className="text-pink-base">
+            Hello world!
+          </Text>
+          <Text className="text-green-base">Hello world!</Text>
+          <Text variant="body-md-bold">Hello world!</Text>
+        </div>
+
+        <div className="flex gap-1">
+          <Icon svg={TrashIcon} className="fill-green-base" />
+          <Icon svg={CheckIcon} className="fill-green-dark" />
+          <Icon svg={PlusIcon} className="fill-green-light" />
+          <Icon svg={SpinnerIcon} className="fill-pink-base" />
+          <Icon svg={PencilIcon} className="fill-pink-dark" />
+          <Icon svg={XIcon} className="fill-pink-light" />
+        </div>
+
+        <div className="flex gap-1">
+          <Badge variant="secondary">5</Badge>
+          <Badge variant="primary">2 de 5</Badge>
+          <Badge loading>2 de 5</Badge>
+        </div>
+
+        <div>
+          <Button icon={PlusIcon}>Nova tarefa</Button>
+        </div>
+
+        <div className="flex gap-1">
+          <ButtonIcon icon={TrashIcon} />
+          <ButtonIcon icon={TrashIcon} variant="secondary" />
+          <ButtonIcon icon={TrashIcon} variant="tertiary" />
+          <ButtonIcon icon={TrashIcon} loading />
+        </div>
+
+        <div>
+          <InputText />
+        </div>
+
+        <div className="flex gap-1">
+          <InputCheckbox />
+          <InputCheckbox loading />
+        </div>
+
+        <div>
+          <Card size="md">Hello world!</Card>
+        </div>
+
+        <div className="space-y-2">
+          <Skeleton className="h-6" />
+          <Skeleton className="h-6" />
+          <Skeleton className="h-6 w-96" />
+        </div>
+
+        <div></div>
+
+        <div></div>
+      </div>
+    </Container>
+  );
+}

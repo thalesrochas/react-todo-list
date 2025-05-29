a5fcb9a420789e45933b8acbd285eb92bab1433c
Thales Rocha <thales_rochasousa@hotmail.com>
2025-05-29 00:40:15 -0300

    Adiciona novo componente InputText.

---
+++ src/components/input-text.tsx
@@ -0,0 +1,42 @@
+import { cva, cx, type VariantProps } from "class-variance-authority";
+import type React from "react";
+import { textVariants } from "./text";
+
+export const inputTextVariants = cva(
+  "focus:border-pink-base border-b border-solid border-gray-200 bg-transparent outline-none",
+  {
+    variants: {
+      size: {
+        md: "px-2 pb-2",
+      },
+      disabled: {
+        true: "pointer-events-none",
+      },
+    },
+    defaultVariants: {
+      size: "md",
+      disabled: false,
+    },
+  },
+);
+
+type InputTextProps = VariantProps<typeof inputTextVariants> &
+  Omit<React.ComponentProps<"input">, "size" | "disabled">;
+
+export default function InputText({
+  size,
+  disabled,
+  className,
+  ...props
+}: InputTextProps) {
+  return (
+    <input
+      className={cx(
+        inputTextVariants({ size, disabled }),
+        textVariants(),
+        className,
+      )}
+      {...props}
+    />
+  );
+}

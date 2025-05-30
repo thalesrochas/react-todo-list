8f5a5a1c6cbbb8b2f2236271c496cf755517991c
Thales Rocha <thales_rochasousa@hotmail.com>
2025-05-30 20:38:21 -0300

    Implementa edição de tarefas no componente TaskItem e atualiza a lista de tarefas para desabilitar a criação enquanto uma tarefa está sendo editada. Adiciona a função updateTask no hook useTask para gerenciar a atualização da tarefa.

--- src/core-components/task-item.tsx
+++ src/core-components/task-item.tsx
@@ -11,6 +11,7 @@ import CheckIcon from "../assets/icons/check.svg?react";
 import PencilIcon from "../assets/icons/pencil.svg?react";
 import TrashIcon from "../assets/icons/trash.svg?react";
 import XIcon from "../assets/icons/x.svg?react";
+import useTask from "../hooks/use-task";
 import { TaskState, type Task } from "../models/task";
 
 type TaskItemProps = {
@@ -18,10 +19,12 @@ type TaskItemProps = {
 };
 
 export default function TaskItem({ task }: TaskItemProps) {
-  console.log(task);
   const [isEditing, setIsEditing] = React.useState(
     task?.state === TaskState.Creating,
   );
+
+  const [taskTitle, setTaskTitle] = React.useState(task.title || "");
+  const { updateTask } = useTask();
 
   function handleEditTask() {
     setIsEditing(true);
@@ -31,10 +34,20 @@ export default function TaskItem({ task }: TaskItemProps) {
     setIsEditing(false);
   }
 
+  function handleChangeTaskTitle(e: React.ChangeEvent<HTMLInputElement>) {
+    setTaskTitle(e.target.value || "");
+  }
+
+  function handleSaveTask(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    updateTask(task.id, { title: taskTitle });
+    setIsEditing(false);
+  }
+
   return (
-    <Card size="md" className="flex items-center gap-4">
+    <Card size="md">
       {!isEditing ? (
-        <>
+        <div className="flex items-center gap-4">
           <InputCheckbox
             checked={task?.concluded}
             value={task?.concluded?.toString()}
@@ -50,19 +63,26 @@ export default function TaskItem({ task }: TaskItemProps) {
               onClick={handleEditTask}
             />
           </div>
-        </>
+        </div>
       ) : (
-        <>
-          <InputText className="flex-1" />
+        <form onSubmit={handleSaveTask} className="flex items-center gap-4">
+          <InputText
+            value={taskTitle}
+            className="flex-1"
+            onChange={handleChangeTaskTitle}
+            required
+            autoFocus
+          />
           <div className="flex gap-1">
             <ButtonIcon
+              type="button"
               icon={XIcon}
               variant="secondary"
               onClick={handleExitEditTask}
             />
-            <ButtonIcon icon={CheckIcon} />
+            <ButtonIcon type="submit" icon={CheckIcon} />
           </div>
-        </>
+        </form>
       )}
     </Card>
   );
--- src/core-components/tasks-list.tsx
+++ src/core-components/tasks-list.tsx
@@ -3,6 +3,7 @@ import Button from "../components/button";
 import PlusIcon from "../assets/icons/plus.svg?react";
 import useTask from "../hooks/use-task";
 import useTasks from "../hooks/use-tasks";
+import { TaskState } from "../models/task";
 import TaskItem from "./task-item";
 
 export default function TasksList() {
@@ -16,7 +17,12 @@ export default function TasksList() {
   return (
     <>
       <section>
-        <Button icon={PlusIcon} className="w-full" onClick={handleNewTask}>
+        <Button
+          icon={PlusIcon}
+          className="w-full"
+          onClick={handleNewTask}
+          disabled={tasks.some(task => task.state === TaskState.Creating)}
+        >
           Nota tarefa
         </Button>
       </section>

a76fa2fc67cacf9f179c069a14a43d4cad7d2a91
Thales Rocha <thales_rochasousa@hotmail.com>
2025-05-30 20:08:33 -0300

    Adiciona novos componentes Header, Footer e MainContent. Atualiza o layout da página principal para incluir esses componentes. Exibir a lista e resumo de tarefas na página Home.

--- src/pages/layout-main.tsx
+++ src/pages/layout-main.tsx
@@ -1,33 +1,18 @@
-import { NavLink, Outlet } from "react-router";
-import Container from "../components/container";
-import Text from "../components/text";
+import { Outlet } from "react-router";
+import Footer from "../core-components/footer";
+import Header from "../core-components/header";
+import MainContent from "../core-components/main-content";
 
 export default function LayoutMain() {
   return (
     <>
-      <Container as="header" className="mt-3 md:mt-20">
-        Hello world - HEADER
-      </Container>
+      <Header />
 
-      <main className="mt-4 md:mt-8">
+      <MainContent>
         <Outlet />
-      </main>
+      </MainContent>
 
-      <footer className="my-5 md:my-10">
-        <nav className="flex items-center justify-center gap-4">
-          <NavLink to="/">
-            <Text variant="body-sm-bold" className="text-gray-300">
-              Home
-            </Text>
-          </NavLink>
-
-          <NavLink to="/componentes">
-            <Text variant="body-sm-bold" className="text-gray-300">
-              Componentes
-            </Text>
-          </NavLink>
-        </nav>
-      </footer>
+      <Footer />
     </>
   );
 }

2ba04be268a5a31f220774cbd873b83d37ed6f63
Thales Rocha <thales_rochasousa@hotmail.com>
2025-05-31 16:06:19 -0300

    Atualiza o componente TasksSummary para utilizar o hook useTasks, exibindo a contagem de tarefas criadas e concluídas dinamicamente. Modifica o hook useTasks para calcular a contagem de tarefas criadas com base no estado das tarefas.

--- src/hooks/use-tasks.ts
+++ src/hooks/use-tasks.ts
@@ -1,13 +1,13 @@
 import useLocalStorage from "use-local-storage";
 
-import { TASKS_KEY, type Task } from "../models/task";
+import { TASKS_KEY, TaskState, type Task } from "../models/task";
 
 export default function useTasks() {
   const [tasks] = useLocalStorage<Task[]>(TASKS_KEY, []);
 
   return {
     tasks,
-    tasksCount: tasks.length,
+    tasksCount: tasks.filter(task => task.state === TaskState.Created).length,
     concludedTaskCount: tasks.filter(task => task.concluded).length,
   };
 }
